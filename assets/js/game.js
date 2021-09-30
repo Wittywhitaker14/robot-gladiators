6930693f2f5136088b3db83a1258a9911ff9acad
Austin Whitaker <wittywhitaker14@gmail.com>
2021-09-30 07:13:02 -0600

    Fight functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,20 +1,46 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-function fight() {
-    window.alert("The fight has begun!")
-}
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
 
-var playerName = prompt("What is your robot's name?");
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-console.log(playerName);
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+  
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+  
+    enemyHealth = enemyHealth - playerAttack;
+    
+    // Log a resulting message to the console so we know that it worked.
 
-console.log("This logs a string, good for leaving yourself a message");
+    console.log(
+        playerName + " attacked " + enemyName + "." + enemyName + " now has " + enemyHealth + " health remaining. "
+    );
+    if (enemyHealth <= 0) {
+        alert(enemyName + " has died!");
+    }
+    else {
+        alert(enemyName + " still has " + enemyHealth + " health left. ");
+    }
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
 
-console.log(10+10);
+    playerHealth = playerHealth - enemyAttack;
+  
+    // Log a resulting message to the console so we know that it worked.
 
-console.log("Our robot's name is " + playerName);
+    enemyName + "attacked" + playerName + "." + playerName + "now has" + playerHealth + "health remaining."
+    if (playerHealth <= 0) {
+        alert(playerName + " has died!");
+    }
+    else {
+        alert(playerName + " still has " + playerHealth + " health left.");
+    }
+  };
 
-var name = "your name";
-console.log(name);
 
-// fight ();
+fight();
